67b0bd4097cef90dc5273bfc3b44430b44bfb0c9
udai002 <karumuriudaisai002@gmail.com>
2025-09-11 12:36:36 +0530

    added folders

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Routes>
+      <Route path='/' Component={Home} />
+     </Routes>
+    </>
+  )
+}
+
+export default App

e6d2efe62216b310b294b27e372ad2afef3106aa
Sushant Praveen <sushantpraveen.sushantprem2820@gmail.com>
2025-10-23 16:41:24 +0530

    superadmin-overview

--- src/pages/Overview/Superadmin.jsx
+++ src/pages/Overview/Superadmin.jsx
@@ -1,33 +1,64 @@
-import React from 'react'
-import OverviewComp from '../../components/Overview'
-import total from '../../../public/OverView/Total.png'
-import revenue from '../../../public/OverView/revenue.png'
-import pending from '../../../public/OverView/pending.png'
-import Payment from '../../components/Payment'
-import AnnouncementsOverView from '../../components/AnnouncementsOverView'
-
+import React, { useEffect } from "react";
+import OverviewComp from "../../components/Overview";
+import total from "../../../public/OverView/Total.png";
+import revenue from "../../../public/OverView/revenue.png";
+import pending from "../../../public/OverView/pending.png";
+import Payment from "../../components/Payment";
+import AnnouncementsOverView from "../../components/AnnouncementsOverView";
+import { useState } from "react";
 
 export default function Superadmin() {
+  const [totalValue, setTotalValue] = useState(0);
+
+  useEffect(() => {
+    async function fetchIntern() {
+      try {
+        const res = await fetch("http://localhost:3000/user/totalintern");
+        const data = await res.json();
+
+        const multiplyTotal = data.total * 100000;
+        setTotalValue(multiplyTotal);
+      } catch (error) {
+        console.error("Error fetching intern count:", error);
+      }
+    }
+    
+    fetchIntern(); 
+  }, []); 
   return (
-    <div className='w-full hpx-4 py-6'>
-      <div className='flex gap-2'>
-        <OverviewComp title="Total Target" revenue="Rs 1,00,000" img={total} className="  "></OverviewComp>
-        <OverviewComp title="Projected Revenue" revenue="Rs 1,00,000" img={total}></OverviewComp>
-        <OverviewComp title="Revenue Credited" revenue="Rs 50,000" img={revenue}></OverviewComp>
-        <OverviewComp title="Pending Revenue" revenue="Rs 50,000" img={pending}></OverviewComp>
+    <div className="w-full hpx-4 py-6">
+      <div className="flex gap-2">
+        <OverviewComp
+          title="Total Target"
+          revenue={`Rs ${totalValue.toLocaleString()}`} 
+          img="/OverView/revenue.png"
+          className="  "
+        ></OverviewComp>
+        <OverviewComp
+          title="Projected Revenue"
+          revenue="Rs 1,00,000"
+          img={total}
+        ></OverviewComp>
+        <OverviewComp
+          title="Revenue Credited"
+          revenue="Rs 50,000"
+          img={revenue}
+        ></OverviewComp>
+        <OverviewComp
+          title="Pending Revenue"
+          revenue="Rs 50,000"
+          img={pending}
+        ></OverviewComp>
       </div>
-      <div className='flex  mt-3 gap-3'>
-        <div className='flex-col'>
+      <div className="flex  mt-3 gap-3">
+        <div className="flex-col">
           <OverviewComp title="Total Payment counts" revenue="50" />
           <Payment />
-
         </div>
-        <div className='w-full'>
+        <div className="w-full">
           <AnnouncementsOverView />
         </div>
       </div>
-
-
     </div>
-  )
+  );
 }
